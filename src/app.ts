c01ce1fc55c8bb68c482fa15624cc68a73d00ab6
Mehedi Hassan <mdmehedihassan0022@gmail.com>
2026-08-28 02:58:43 +0600

    Update To Prisma 8

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ import cookieParser from "cookie-parser";
 import routes from "./routes/index.js";
 import { notFoundHandler } from "./middlewares/notFound.middleware.js";
 import { globalErrorHandler } from "./middlewares/error.middleware.js";
-import { prisma } from "./lib/prisma.js";
 import { asyncHandler } from "./utils/asyncHandler.js";
 
 const app = express();
@@ -23,17 +22,6 @@ app.use(
 
 app.use(cookieParser());
 
-app.get(
-  "/db-health",
-  asyncHandler(async (req, res) => {
-    await prisma.$queryRaw`SELECT 1`;
-
-    res.status(200).json({
-      success: true,
-      message: "Database connection is healthy",
-    });
-  }),
-);
 app.use("/api/v1", routes);
 
 app.use(notFoundHandler);
